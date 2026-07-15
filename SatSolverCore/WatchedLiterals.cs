4fcb2fff349e6495558e35e17b908220c0e1395f
Aleksi Romppainen <aromppa@gmail.com>
2026-07-15 17:32:29 +0300

    Fix issue where the same literal was propagated multiple times

--- SatSolverCore/WatchedLiterals.cs
+++ SatSolverCore/WatchedLiterals.cs
@@ -30,7 +30,7 @@ public class WatchedLiterals
 
     public bool TryAssignToFalse(int literal, PartialAssignment assignment, out List<int> propagated)
     {
-        propagated = [];
+        HashSet<int> result = [];
 
         if (watchlist.TryGetValue(literal, out var list))
         {
@@ -49,7 +49,7 @@ public class WatchedLiterals
 
                 if (propagate != 0)
                 {
-                    propagated.Add(propagate);
+                    result.Add(propagate);
                 }
 
                 if (watched != 0)
@@ -66,6 +66,7 @@ public class WatchedLiterals
             }
         }
 
+        propagated = [.. result];
         return true;
     }
 }
