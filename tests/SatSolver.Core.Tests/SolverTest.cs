97455f29ecda7cfff53715ee6653811b7aab9ae5
Aleksi Romppainen <aromppa@gmail.com>
2026-08-05 20:35:49 +0300

    Add unit test for Solver timeout

---
+++ tests/SatSolver.Core.Tests/SolverTest.cs
@@ -0,0 +1,24 @@
+using SatSolver.Shared;
+
+namespace SatSolver.Core.Tests;
+
+[Trait("Category", "Unit")]
+public class SolverTest
+{
+    [Fact]
+    public void Solve_ShouldReturnUnknownResult_WhenTimeoutHappens()
+    {
+        Formula formula = new("test", 5, [
+            [1, 2, 3],
+            [2, 3, 4],
+            [3, 4, 5],
+            [-1, -2, -3],
+            [-2, -3, -4],
+            [-3, -4, -5],
+        ]);
+
+        SolveResult result = new Solver(formula).Solve(new(0, 0, 0));
+
+        Assert.Equal(SolveResult.ResultType.UNKNOWN, result.Type);
+    }
+}
