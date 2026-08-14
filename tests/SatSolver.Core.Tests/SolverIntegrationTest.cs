161ca9e73319f39fed57ef3988ac7d807e27aa71
Aleksi Romppainen <aromppa@gmail.com>
2026-08-14 18:11:11 +0300

    Update test cases

--- tests/SatSolver.Core.Tests/SolverIntegrationTest.cs
+++ tests/SatSolver.Core.Tests/SolverIntegrationTest.cs
@@ -7,20 +7,44 @@ public class SolverIntegrationTest
 {
     [Theory]
     [DimacsFileData("testdata/kissat/sat")]
-    public void TestSolverKissatSat(Formula formula)
+    public void Solve_ShouldReturnSatisfiableResult_WhenFormulaIsSatisfiable(Formula formula)
     {
         SolveResult result = new Solver(formula).Solve();
 
         Assert.Equal(SolveResultType.SATISFIABLE, result.Type);
         Assert.Equal(formula.NumberOfVars, result.Assignment.Count);
+
+        // Assert that the formula is really satisfied, i.e. the truth assignment τ contains
+        // at least one literal l per clause such that τ(l) = TRUE.
+        foreach (List<int> clause in formula.Clauses)
+        {
+            Assert.Contains(clause, result.Assignment.Contains);
+        }
     }
 
     [Theory]
     [DimacsFileData("testdata/kissat/unsat")]
-    public void TestSolverKissatUnsat(Formula formula)
+    public void Solve_ShouldReturnUnsatisfiableResult_WhenFormulaIsNotSatisfiable(Formula formula)
     {
         SolveResult result = new Solver(formula).Solve();
 
         Assert.Equal(SolveResultType.UNSATISFIABLE, result.Type);
+    }
+
+    [Fact]
+    public void Solve_ShouldReturnUnknownResult_WhenTimeoutHappens()
+    {
+        Formula formula = new("test", 5, [
+            [1, 2, 3],
+            [2, 3, 4],
+            [3, 4, 5],
+            [-1, -2, -3],
+            [-2, -3, -4],
+            [-3, -4, -5],
+        ]);
+
+        SolveResult result = new Solver(formula).Solve(new(0, 0, 0));
+
+        Assert.Equal(SolveResultType.UNKNOWN, result.Type);
     }
 }
